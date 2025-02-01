0462080b3eb3bf05df4dc484ffc383dc6b0f96b4
dhruvbcodes <dbaheti2003@gmail.com>
2025-02-02 00:05:44 +0530

    deployment config

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pip install -r requirements.txt
-python -m spacy download en_core_web_sm
+python -m spacy download en_core_web_sm_trf
 python -c "import nltk; nltk.download('punkt')"
 gunicorn -k uvicorn.workers.UvicornWorker Sentiment:app --bind 0.0.0.0:$PORT
