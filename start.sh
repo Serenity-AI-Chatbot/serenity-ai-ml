499cda0fde3c4a2135714f08d580188dc261d594
dhruvbcodes <dbaheti2003@gmail.com>
2025-02-02 15:20:54 +0530

    deployment config

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pip install --upgrade pip
 pip install -r requirements.txt
-python -m spacy download xx_ent_wiki_sm
+python -m spacy download en_core_web_sm
 python -c "import nltk; nltk.download('punkt')"
